c940dbed7c95ebbb602c760f4da8f389af3e4674
antonymawork <139542980+antonymawork@users.noreply.github.com>
2024-05-14 16:34:36 +0800

    Update HTTP_main.py

--- HTTP_main.py
+++ HTTP_main.py
@@ -4,19 +4,30 @@ import json
 import redis
 
 # Create Redis connection
-redis_connection = redis.Redis()
+REDIS_HOST = '44.215.160.99'
+REDIS_PORT = 6379
+REDIS_DB = 0
+
+r = redis.Redis(host=REDIS_HOST, 
+            port=REDIS_PORT, 
+            db=REDIS_DB, 
+            password=ierg4080, 
+            health_check_interval=10,
+            socket_timeout=1000, socket_keepalive=True,
+            socket_connect_timeout=1000, retry_on_timeout=True
+            )
 
 def listen_predictions():
     while True:
         # Now start processing prediction tasks
-        _, message = redis_connection.blpop("prediction")
+        _, message = r.blpop("prediction")
         print(f"Received message from Redis: {message}")
         task = json.loads(message)
         print(f"Received task: {task}")
         task_id = task.get("task_id")
         redis_key = f"result:{task_id}"
         redis_value = json.dumps(task)
-        redis_connection.set(redis_key, redis_value)
+        r.set(redis_key, redis_value)
         for prediction in task['predictions']:
             print("Language: {}, Score: {:.4f}".format(prediction['label'], prediction['score']))
 
